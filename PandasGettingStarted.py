9ed1b0114c4bc72dbea078304c57304b86938e08
chisoftltd <chisoftltd@gmail.com>
2021-02-25 17:27:05 +0000

    first commit

---
+++ PandasGettingStarted.py
@@ -0,0 +1,25 @@
+# Pandas Getting Started
+# Import Pandas
+import pandas
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pandas.DataFrame(mydataset)
+
+print(myvar)
+print()
+
+# Pandas as pd
+import pandas as pd 
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+myvar = pd.DataFrame(mydataset)
+print(myvar)
+print()
+
+# Checking Pandas Version
+print(pd.__version__)
